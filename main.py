8cf54cb6b8bfb53d381926f1262bf6ec568c1507
yakuzzaa <chornaya.gryazz@gmail.com>
2023-06-08 01:51:18 +0400

    new feature

--- main.py
+++ main.py
@@ -12,8 +12,6 @@
     for i in reader.generator():
         print(i)
 
-    print('=' * 20)
-
     fio, grant, destination = input('Введите новые данные(ФИО, стипендия, назначение справки:
 ').split()
     reader.new_entry(fio, grant, destination)
