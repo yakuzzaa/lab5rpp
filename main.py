aa398b617f5c678a91151df0efaf52a43ca79a7e
yakuzzaa <chornaya.gryazz@gmail.com>
2023-06-07 21:29:31 +0400

    git init

---
+++ main.py
@@ -0,0 +1,20 @@
+ print(f"Сортировка по стипендии: {reader.grant_sorting('grant')}")
+    print(f"Сортировка по стипендии, которая больше value
+{reader.grant_more_than_sorting('grant','2000')}")
+
+    print("Итерация")
+    for i in reader:
+        print(i)
+
+    print('=' * 20)
+
+    print('Генератор')
+    for i in reader.generator():
+        print(i)
+
+    print('=' * 20)
+
+    fio, grant, destination = input('Введите новые данные(ФИО, стипендия, назначение справки:
+').split()
+    reader.new_entry(fio, grant, destination)
+    reader.save()
