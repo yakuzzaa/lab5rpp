3564e38c9352101fb91a8f38a491fcea05fb818e
yakuzzaa <chornaya.gryazz@gmail.com>
2023-06-08 01:53:14 +0400

    new feature 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
- print(f"Сортировка по стипендии: {reader.grant_sorting('grant')}")
+ nt(f"Сортировка по стипендии: {reader.grant_sorting('grant')}")
     print(f"Сортировка по стипендии, которая больше value
 {reader.grant_more_than_sorting('grant','2000')}")
 
@@ -12,7 +12,7 @@
     for i in reader.generator():
         print(i)
 
-    print('=' * 20)
+    print('=' * 20000)
 
     fio, grant, destination = input('Введите новые данные(ФИО, стипендия, назначение справки:
 ').split()
