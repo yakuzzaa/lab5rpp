7d4bac277c0ed0748f45cde8d0d0249dd1749afc
yakuzzaa <chornaya.gryazz@gmail.com>
2023-06-08 01:07:57 +0400

    add branch v2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+print("Version 2")
  print(f"Сортировка по стипендии: {reader.grant_sorting('grant')}")
     print(f"Сортировка по стипендии, которая больше value
 {reader.grant_more_than_sorting('grant','2000')}")
@@ -6,13 +7,12 @@
     for i in reader:
         print(i)
 
-    print('=' * 20)
+    print('=' * 25)
 
     print('Генератор')
     for i in reader.generator():
         print(i)
 
-    print('=' * 20)
 
     fio, grant, destination = input('Введите новые данные(ФИО, стипендия, назначение справки:
 ').split()
